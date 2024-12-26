faec1f2318a8a3d4a61372d77152a2ff7ca12674
lokeshloki3 <lokesh1992p@gmail.com>
2024-12-26 18:54:32 +0530

    comments created

--- routes/blog.js
+++ routes/blog.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 // import controller
-// const { dummyLink} = require('../controllers/likeController');
+const { dummyLink} = require('../controllers/likeController');
 const { createComment } = require('../controllers/commentController');
 
 // Mapping created
-// router.get('/dummyroute', dummyLink);
+router.get('/dummyroute', dummyLink);
 router.post('/comments/create', createComment);
 
 // export
