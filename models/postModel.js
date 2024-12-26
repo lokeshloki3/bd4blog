d757fbef3595942d2a3c947b42ddabad9f2766b0
lokeshloki3 <lokesh1992p@gmail.com>
2024-12-26 17:55:03 +0530

    Model Schema created

---
+++ models/postModel.js
@@ -0,0 +1,30 @@
+//import mongoose
+const mongoose = require("mongoose");
+
+// route handler
+const postSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  body: {
+    type: String,
+    required: true,
+  },
+  // Square brackets in likes and comments are because they contain array of likes and comments
+  likes: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Like",
+    },
+  ],
+  comments: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Comment",
+    },
+  ],
+});
+
+// export
+module.exports = mongoose.model("Post", postSchema);
