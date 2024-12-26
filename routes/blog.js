da56c634b60b5f3a2026a3a9cca4f8830d0c70c3
lokeshloki3 <lokesh1992p@gmail.com>
2024-12-26 19:00:31 +0530

    Post created added

--- routes/blog.js
+++ routes/blog.js
@@ -4,10 +4,12 @@ const router = express.Router();
 // import controller
 const { dummyLink} = require('../controllers/likeController');
 const { createComment } = require('../controllers/commentController');
+const { createPost } = require('../controllers/postController');
 
 // Mapping created
 router.get('/dummyroute', dummyLink);
 router.post('/comments/create', createComment);
+router.post('/posts/create', createPost);
 
 // export
 module.exports = router;
