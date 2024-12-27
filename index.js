0d8f52ba85e33d2f0a97226f7dbe709af3b5e28e
lokeshloki3 <lokesh1992p@gmail.com>
2024-12-27 11:26:17 +0530

    comments added

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 const blog = require("./routes/blog");
-// mount
+// mount - addition /api/v1 and routes
 app.use("/api/v1", blog);
 
 const connectWithDb = require("./config/database");
